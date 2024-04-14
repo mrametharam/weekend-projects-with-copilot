0de05e928a6dbace0a7e93755dfc3b0aeb7f753b
Martin Metharam <mam0520@carenethealthcare.com>
2024-04-14 16:13:35 -0500

    Further refactored the ApiService class.

--- photo-fetcher/PhotoFetcherSolution/PhotoFetcherMonolithicConsoleApplication/TheApiService/v2/ApiService.cs
+++ photo-fetcher/PhotoFetcherSolution/PhotoFetcherMonolithicConsoleApplication/TheApiService/v2/ApiService.cs
@@ -1,92 +1,79 @@
-﻿using System.Data.SqlClient;
+﻿using PhotoFetcherMonolithicConsoleApplication.TheApiService.v2.Data.Photo;
+using PhotoFetcherMonolithicConsoleApplication.TheApiService.v2.Core;
 using System.Diagnostics;
 using System.Text.Json;
-using Dapper;
-using PhotoFetcherMonolithicConsoleApplication.TheApiService.v2.Core;
-using PhotoFetcherMonolithicConsoleApplication.TheApiService.v2.Data.Photo;
 
 namespace PhotoFetcherMonolithicConsoleApplication.TheApiService.v2;
 
 internal class ApiService
 {
+    HttpClient _httpClient = new HttpClient();
+    List<Domain.Models.Photo> _photos = new();
+
     internal async Task ExecuteAsync()
     {
-        HttpClient httpClient = new HttpClient();
-        List<Domain.Models.Photo> photos = new();
-
         // TODO: Update the exception handlers to be more specific.
         // TODO: Build a logger class that performs the console.Writeline and the stopwatch tasks.
         // TODO: Look at how the chain of responsibility pattern can be used to perform the API call, validation of the data, then the Saving to the DB.
 
+        string elapsedTime = "";
+        var stopWatch = new Stopwatch();
+
+        stopWatch.Start();
+
+        for (int id = 2500; id <= 2520; id++)
+        {
+            await FetchData(id);
+
+            if (id % 20 == 0)
+            {
+                await Task.Delay(3000);
+            }
+        }
+
+        foreach (var photo in _photos)
+        {
+            var saveRec = PhotoDbMapper.MapFromPhoto(photo, DateTime.UtcNow, "");
+
+            var photoRepo = new PhotosRepository();
+            await photoRepo.Insert(saveRec);
+
+            Console.WriteLine($"Saved photo: {saveRec.PhotoId} / {_photos.Count}");
+        }
+
+        TimeSpan ts = stopWatch.Elapsed;
+        elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+
+        Console.WriteLine($"Elapsed Time: {elapsedTime}");
+        Console.WriteLine("\n====================\n");
+
+        stopWatch.Stop();
+
+        Console.WriteLine($"Total: {_photos.Count},  Completed in: {elapsedTime}");
+    }
+
+    private async Task FetchData(int id)
+    {
         try
         {
-            string elapsedTime = "";
-            var stopWatch = new Stopwatch();
+            using HttpResponseMessage response = await _httpClient.GetAsync($"{Configuration.PhotosApi.Url}/{id}");
 
-            stopWatch.Start();
+            var responseCode = (int)response.StatusCode;
+            var responseBody = await response.Content.ReadAsStringAsync();
 
-            for (int id = 2000; id <= 5000; id++)
+            Console.WriteLine($"{responseCode}: {responseBody}");   // TODO: Move this into a logging class.
+
+            if (response.IsSuccessStatusCode)
             {
-                #region TODO: Move this into a service that will fetch the data and save it to a list.
-                using HttpResponseMessage response = await httpClient.GetAsync($"{Configuration.PhotosApi.Url}/{id}");
-
-                var responseCode = (int)response.StatusCode;
-                var responseBody = await response.Content.ReadAsStringAsync();
-
-                string result = "";
-
-                if (response.IsSuccessStatusCode)
+                var options = new JsonSerializerOptions
                 {
-                    result = $"SUCCESS!\nHttpCode: {responseCode}\nResponse Body:{responseBody}";
+                    PropertyNameCaseInsensitive = true
+                };
 
-                    var options = new JsonSerializerOptions
-                    {
-                        PropertyNameCaseInsensitive = true
-                    };
+                var photo = JsonSerializer.Deserialize<Domain.Models.Photo>(responseBody, options) ?? new Domain.Models.Photo();
 
-                    var photo = JsonSerializer.Deserialize<Domain.Models.Photo>(responseBody, options) ?? new Domain.Models.Photo();
-
-                    photos.Add(photo);
-                    #endregion
-
-                    Console.WriteLine($"Record:: Id: {photo.Id}, Title: {photo.Title}, Album Id: {photo.AlbumId}, Url: {photo.Url}, Thumbnail Url: {photo.ThumbnailUrl}\n");
-
-                    string sql = "";
-
-                    try
-                    {
-                        var saveRec = PhotoDbMapper.MapFromPhoto(photo, DateTime.UtcNow, responseBody);
-
-                        var photoRepo = new PhotosRepository();
-                        await photoRepo.Insert(saveRec);
-
-                        Console.WriteLine($"SAVED! PhotoId: {saveRec.PhotoId}, Title: {saveRec.Title}, Url: {saveRec.Url}");
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine($"Failed to write to the Db.\nException: {ex.Message}\nSQL: {sql}");
-                    }
-                }
-                else
-                {
-                    result = $"FAILED!\nHttpCode: {responseCode}\nResponse Body:{responseBody}";
-                }
-
-                TimeSpan ts = stopWatch.Elapsed;
-                elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
-
-                Console.WriteLine(result + $"\nElapsed Time: {elapsedTime}");
-                Console.WriteLine("\n====================\n");
-
-                if (id % 20 == 0)
-                {
-                    await Task.Delay(3000);
-                }
+                _photos.Add(photo);
             }
-
-            stopWatch.Stop();
-
-            Console.WriteLine($"Total: {photos.Count},  Completed in: {elapsedTime}");
         }
         catch (Exception ex)
         {
